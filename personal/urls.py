05609b320df65d30c8e8e262945b5e8a140649a2
David Jung <d.jung460@gmail.com>
2016-12-25 15:50:34 -0800

    Refactor to use jinja logic

--- personal/urls.py
+++ personal/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^/blog/$', views.blog, name='blog'),
 ]
 
--- personal/views.py
+++ personal/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-	return render(request, 'personal/index.html')
+	return render(request, 'personal/home.html')
+
+def blog(request):
+	return render(request, 'personal/blog.html')
