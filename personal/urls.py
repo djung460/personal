3e3c7bf87a0a6750cf442dd122c205dba9d943fb
David Jung <d.jung460@gmail.com>
2016-12-26 21:33:34 -0800

    Add blog portion and html5 canvas

--- personal/urls.py
+++ personal/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^/blog/$', views.blog, name='blog'),
+    url(r'^blog/', views.blog, name='blog'),
 ]
 
