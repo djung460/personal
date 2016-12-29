c911d908d3527e5dd0e7ac537bccb99711fea402
David Jung <d.jung460@gmail.com>
2016-12-28 20:03:40 -0800

    Add simple Bokeh graph

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.resources import CDN
+
+
+def simple_graph(request):
+    plot = figure()
+    plot.circle([1, 2], [3, 4])
+
+    plot.circle()
+
+    script, div = components(plot, CDN)
+
+    return render(request, 'dashboard/dashboard.html',
+                  {'dashboard_script': script, 'plot_div': div})
